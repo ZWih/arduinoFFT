6cd75113b3ec0f45be876937c1590f3ca31d0458
Zack <zwihela2020@my.fit.edu>
2023-04-26 11:20:27 -0400

    Fixed major peaks

--- src/MarduinoFFT.h
+++ src/MarduinoFFT.h
@@ -129,9 +129,9 @@ public:
   void ComplexToMagnitude();
   void Compute(FFTDirection dir);
   void DCRemoval();
-  double MajorPeak(double[] a);
-  double MajorPeak2(double[] a);
-  double MajorPeak3(double[] a);
+  double MajorPeak(double a[]);
+  double MajorPeak2(double a[]);
+  double MajorPeak3(double a[]);
 
   void MajorPeak(double *f, double *v);
   void Windowing(FFTWindow windowType, FFTDirection dir);
